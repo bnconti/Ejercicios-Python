4f50e04cbb4c7e6db0987b8421544b418dadb33b
bnconti <bruno.n.conti@gmail.com>
2017-07-26 12:10:40 -0300

    Update and rename 3_seconds2years.py to ch1/3_seconds2years.py

---
+++ ch1/3_seconds2years.py
@@ -0,0 +1,11 @@
+# Exercise 1.3: Derive and compute a formula
+
+"""
+SAMPLE RUN:
+Norwegian baby's life expectative: 32 years.
+"""
+
+seconds = 10**9
+years = seconds/60/60/24/365
+
+print ("Norwegian baby's life expectative: {:.0f} years.".format(years))
