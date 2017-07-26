9e59e8c1b8a8d5114ee1994b9cf19163ff68c789
bnconti <bruno.n.conti@gmail.com>
2017-07-26 12:05:16 -0300

    Update and rename 18_find_errors_tan.py to ch1/18_find_errors_tan.py

--- ch1/18_find_errors_tan.py
+++ ch1/18_find_errors_tan.py
@@ -1,6 +1,13 @@
 # Exercise 1.18: Find errors in a program
 
+""" 
+SAMPLE RUN:
+1.00 
+1.73
+"""
+
 from math import pi, tan
+
 tan1 = tan(pi/4)
 tan2 = tan(pi/3)
 print ("{:.2f} \n{:.2f}".format(tan1, tan2))
