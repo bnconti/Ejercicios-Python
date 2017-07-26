131fd348d3fe1a03fff25f1078d057cbde9d4c4d
bnconti <bruno.n.conti@gmail.com>
2017-07-26 12:06:23 -0300

    Update and rename 1_1plus1.py to ch1/1_1plus1.py

--- 1_1plus1.py
+++
@@ -1,9 +0,0 @@
-"""
-Exercise 1.1: Compute 1+1
-The first exercise concerns some very basic mathematics and programming: assign
-the result of 1C1 to a variable and print the value of that variable.
-Filename: 1plus1 .
-"""
-
-resultado = 1 + 1
-print (resultado)
---
+++ ch1/1_1plus1.py
@@ -0,0 +1,9 @@
+# Exercise 1.1: Compute 1+1
+
+"""
+SAMPLE RUN:
+2
+"""
+
+resultado = 1 + 1
+print (resultado)
