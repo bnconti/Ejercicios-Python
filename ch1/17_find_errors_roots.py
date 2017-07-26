73bac2cd032cb17f3776d487d39b8fc4068779a7
bnconti <bruno.n.conti@gmail.com>
2017-07-26 12:02:03 -0300

    Update and rename 17_find_errors_roots.py to ch1/17_find_errors_roots.py

---
+++ ch1/17_find_errors_roots.py
@@ -0,0 +1,15 @@
+# Exercise 1.17: Find errors in the coding of a formula
+
+# The problem is that the program is trying to solve a negative square root.
+
+# I think it gets fixed by multiplying q_sr by j (complex number) and doing the
+# square root of the negation of q, but I am not sure.
+
+from math import sqrt
+
+a = 2; b = 1; c = 2
+q = b*b - 4*a*c
+q_sr = sqrt(-q)*1j
+x1 = (-b + q_sr)/(2*a)
+x2 = (-b - q_sr)/(2*a)
+print (x1); print(x2)
