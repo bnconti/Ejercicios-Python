c324e3f70706be1c3117a10114105ec1f6bcad89
bnconti <bruno.n.conti@gmail.com>
2017-07-26 12:07:23 -0300

    Update and rename 2_hello_world.py to ch1/2_hello_world.py

--- 2_hello_world.py
+++
@@ -1,8 +0,0 @@
-"""
-Exercise 1.2: Write a Hello World program
-Almost all books about programming languages start with a very simple program
-that prints the text Hello, World! to the screen. Make such a program in Python.
-Filename: hello_world.
-"""
-
-print("Hola, mundo!")
---
+++ ch1/2_hello_world.py
@@ -0,0 +1,8 @@
+# Exercise 1.2: Write a Hello World program
+
+"""
+SAMPLE RUN:
+Hola, mundo!
+"""
+
+print("Hola, mundo!")
