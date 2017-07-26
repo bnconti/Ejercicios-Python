356698f55b584c0c60209484ad94d4798e422ac5
bnconti <bruno.n.conti@gmail.com>
2017-07-26 11:54:50 -0300

    Update and rename 13_trajectory.py to ch1/13_trajectory.py

--- ch1/13_trajectory.py
+++ ch1/13_trajectory.py
@@ -1,5 +1,7 @@
-"""Exercise 1.13: Derive the trajectory of a ball
+# Exercise 1.13: Derive the trajectory of a ball
 
+"""
 The purpose of this exercise is to explain how Equation (1.6) for the trajectory of
 a ball arises from basic physics. There is no programming in this exercise, just
-physics and mathematics."""
+physics and mathematics.
+"""
