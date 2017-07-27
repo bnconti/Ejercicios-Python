a464afe6f605a84c829608c2bddc3f424c1f8347
bnconti <bruno.n.conti@gmail.com>
2017-07-27 20:58:03 -0300

    Update 10_gaussian1.py

--- ch1/10_gaussian1.py
+++ ch1/10_gaussian1.py
@@ -1,11 +1,14 @@
 # Exercise 1.10: Evaluate a Gaussian function
 
+"""
+SAMPLE RUN:
+Result: 0.1210
+"""
+
 from math import pi, sqrt, exp
 
-m = 0
-s = 2
-x = 1
+m = 0; s = 2; x = 1
 
 r = float((1/(sqrt(2*pi)*s))*exp(-0.5*((x-m)/s))**2)
 
-print("Resultado: {:.4f}".format(r))
+print("Result: {:.4f}".format(r))
